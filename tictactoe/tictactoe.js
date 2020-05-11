3a30e48a651559fd11fb1bd38394c24077f61603
Sarcastic-Fringehead <64106614+Sarcastic-Fringehead@users.noreply.github.com>
2020-05-11 12:16:49 -0700

    Update tictactoe.js
    fixed bugs and added a bit of computer intelligence

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -10,34 +10,52 @@ function newGame() {
 	for (var i = 0; i < idNames.length; i++) {
 		document.getElementById(idNames[i]).innerHTML = "";
 	}//for
-	
+
 	//resets variables
 	turns = 0;
 	currentPlayer = "X";
 	gameStatus = "";
 
-	//hides restart button and removes blocker
+	//hides restart button and removes blocker if still up
 	changeVis("controls");
-	changeVis("blocker");
+	document.getElementById("blocker").className = "hidden";
 
 }//newGame
 
 //randomly chooses a free box for computer
 function computerTakeTurn() {
 
-	let idName = "";
+	let idName = "";//name of square to be selected
 
-	//choose random boxes til an empty one is found
-	do{
-		let rand = (parseInt(Math.random()*9)); // 1-9
-		idName =  idNames[rand];
+	//1 in 2 chance that computer checks if opponent has winning moves and stops them
+	if ((parseInt(Math.random()*2)) == 0) {
+		idName = stopWinningMove();
+	}//if
 
-		//check if chosen box is empty
-		if(document.getElementById(idName).innerHTML == "") {
-			document.getElementById(idName).innerHTML = currentPlayer;
-			break;
-		}//if
-	} while(true);
+	//checks if there's any winning moves to be made and assigns them to 
+	//idName, otherwise idName stays the same
+	let move = winningMove();//name of value to assign to idName
+
+	if (move != "") {
+		idName = move;
+	}//if
+	
+
+	//if no smart moves are made, chooses random boxes until an empty one is found
+	if (idName == "") {
+
+		do{
+			let rand = (parseInt(Math.random()*9)); // 0-8
+			idName =  idNames[rand];
+
+			//check if chosen box is empty
+			if(document.getElementById(idName).innerHTML == "") {
+				break;
+			}//if
+		} while(true);
+	}//if
+	//fills in selected box
+	document.getElementById(idName).innerHTML = currentPlayer;
 
 }//computerTakeTurn
 
@@ -58,21 +76,13 @@ function playerTurn(e) {
 
 	// if game is not over, computer goes
 	if (gameStatus == "") {
-
 		setTimeout(function() {
-			changeVis("blocker");
 			computerTakeTurn();
+			changeVis("blocker");
 			checkGameStatus();
 		}, 500
 		);
-	}//if
 
-	//game is over
-	if (gameStatus != ""){
-		setTimeout(function() {
-			showLightBox(gameStatus, "GAME OVER!");
-		}, 500
-		);
 	}//if
 
 } // playerTurn
@@ -86,19 +96,25 @@ function checkGameStatus(){
 	//check for winner
 
 	if (checkWin()) {
-		gameStatus = (currentPlayer == "X" ? "X wins! X-ellent!" : "O wins! O YEAH!");
-		return gameStatus;
+		gameStatus = (currentPlayer == "X"?"X wins! X-ellent!":"O wins! O YEAH!");
+	} else {
+		//check for tie
+		if (turns == 9) {
+			gameStatus = "Tic Tac TIE";
+		}// if
 	}//if
 
 
-	//check for tie
-	if (turns == 9) {
-		gameStatus = "Tic Tac TIE";
-		return gameStatus;
-	}// if
-
 	//switch current player
 	currentPlayer = (currentPlayer == "X" ? "O" : "X" );
+
+	//game is over
+	if (gameStatus != "") {
+		setTimeout(function() {
+			showLightBox(gameStatus, "GAME OVER!");
+		}, 500
+		);
+	}//if
 
 } // checkGameStatus
 
@@ -185,3 +201,220 @@ function continueGame() {
 	}//if
 
 }//continueGame
+
+function winningMove() {
+	let cb = []; //current board
+	cb[0] = ""; // not going to use
+	cb[1] = document.getElementById("one").innerHTML;
+	cb[2] = document.getElementById("two").innerHTML;
+	cb[3] = document.getElementById("three").innerHTML;
+	cb[4] = document.getElementById("four").innerHTML;
+	cb[5] = document.getElementById("five").innerHTML;
+	cb[6] = document.getElementById("six").innerHTML;
+	cb[7] = document.getElementById("seven").innerHTML;
+	cb[8] = document.getElementById("eight").innerHTML;
+	cb[9] = document.getElementById("nine").innerHTML;
+
+	//top row
+	if(cb[1] == "O" && cb[2] == "O" && cb[3] == ""){
+		return "three";
+	}//if
+	if(cb[1] == "O" && cb[2] == "" && cb[3] == "O"){
+		return "two";
+	}//if
+	if(cb[1] == "" && cb[2] == "O" && cb[3] == "O"){
+		return "one";
+	}//if
+
+	//middle row
+	if(cb[4] == "O" && cb[5] == "O" && cb[6] == ""){
+		return "six";
+	}//if
+	if(cb[4] == "O" && cb[5] == "" && cb[6] == "O"){
+		return "five";
+	}//if
+	if(cb[4] == "" && cb[5] == "O" && cb[6] == "O"){
+		return "four";
+	}//if
+
+	//bottom row
+	if(cb[7] == "O" && cb[8] == "O" && cb[9] == ""){
+		return "nine";
+	}//if
+	if(cb[7] == "O" && cb[8] == "" && cb[9] == "O"){
+		return "eight";
+	}//if
+	if(cb[7] == "" && cb[8] == "O" && cb[9] == "O"){
+		return "seven";
+	}//if
+
+
+
+	//left column
+	if(cb[1] == "O" && cb[4] == "O" && cb[7] == ""){
+		return "seven";
+	}//if
+	if(cb[1] == "O" && cb[4] == "" && cb[7] == "O"){
+		return "four";
+	}//if
+	if(cb[1] == "" && cb[4] == "O" && cb[7] == "O"){
+		return "one";
+	}//if
+
+	//center column
+	if(cb[2] == "O" && cb[5] == "O" && cb[8] == ""){
+		return "eight";
+	}//if
+	if(cb[2] == "O" && cb[5] == "" && cb[8] == "O"){
+		return "five";
+	}//if
+	if(cb[2] == "" && cb[5] == "O" && cb[8] == "O"){
+		return "two";
+	}//if
+
+	//right column
+	if(cb[3] == "O" && cb[6] == "O" && cb[9] == ""){
+		return "nine";
+	}//if
+	if(cb[3] == "O" && cb[6] == "" && cb[9] == "O"){
+		return "six";
+	}//if
+	if(cb[3] == "" && cb[6] == "O" && cb[9] == "O"){
+		return "three";
+	}//if
+
+
+
+	//top left to bottom right
+	if(cb[1] == "O" && cb[5] == "O" && cb[9] == ""){
+		return "nine";
+	}//if
+	if(cb[1] == "O" && cb[5] == "" && cb[9] == "O"){
+		return "five";
+	}//if
+	if(cb[1] == "" && cb[5] == "O" && cb[9] == "O"){
+		return "one";
+	}//if
+
+	//top right to bottom left
+	if(cb[3] == "O" && cb[5] == "O" && cb[7] == ""){
+		return "seven";
+	}//if
+	if(cb[3] == "O" && cb[5] == "" && cb[7] == "O"){
+		return "five";
+	}//if
+	if(cb[3] == "" && cb[5] == "O" && cb[7] == "O"){
+		return "three";
+	}//if
+
+
+	return "";
+}//winningMove
+
+function stopWinningMove() {
+	let cb = []; //current board
+	cb[0] = ""; // not going to use
+	cb[1] = document.getElementById("one").innerHTML;
+	cb[2] = document.getElementById("two").innerHTML;
+	cb[3] = document.getElementById("three").innerHTML;
+	cb[4] = document.getElementById("four").innerHTML;
+	cb[5] = document.getElementById("five").innerHTML;
+	cb[6] = document.getElementById("six").innerHTML;
+	cb[7] = document.getElementById("seven").innerHTML;
+	cb[8] = document.getElementById("eight").innerHTML;
+	cb[9] = document.getElementById("nine").innerHTML;
+
+	//top row
+	if(cb[1] == "X" && cb[2] == "X" && cb[3] == ""){
+		return "three";
+	}//if
+	if(cb[1] == "X" && cb[2] == "" && cb[3] == "X"){
+		return "two";
+	}//if
+	if(cb[1] == "" && cb[2] == "X" && cb[3] == "X"){
+		return "one";
+	}//if
+
+	//middle row
+	if(cb[4] == "X" && cb[5] == "X" && cb[6] == ""){
+		return "six";
+	}//if
+	if(cb[4] == "X" && cb[5] == "" && cb[6] == "X"){
+		return "five";
+	}//if
+	if(cb[4] == "" && cb[5] == "X" && cb[6] == "X"){
+		return "four";
+	}//if
+
+	//bottom row
+	if(cb[7] == "X" && cb[8] == "X" && cb[9] == ""){
+		return "nine";
+	}//if
+	if(cb[7] == "X" && cb[8] == "" && cb[9] == "X"){
+		return "eight";
+	}//if
+	if(cb[7] == "" && cb[8] == "X" && cb[9] == "X"){
+		return "seven";
+	}//if
+
+
+
+	//left column
+	if(cb[1] == "X" && cb[4] == "X" && cb[7] == ""){
+		return "seven";
+	}//if
+	if(cb[1] == "X" && cb[4] == "" && cb[7] == "X"){
+		return "four";
+	}//if
+	if(cb[1] == "" && cb[4] == "X" && cb[7] == "X"){
+		return "one";
+	}//if
+
+	//center column
+	if(cb[2] == "X" && cb[5] == "X" && cb[8] == ""){
+		return "eight";
+	}//if
+	if(cb[2] == "X" && cb[5] == "" && cb[8] == "X"){
+		return "five";
+	}//if
+	if(cb[2] == "" && cb[5] == "X" && cb[8] == "X"){
+		return "two";
+	}//if
+
+	//right column
+	if(cb[3] == "X" && cb[6] == "X" && cb[9] == ""){
+		return "nine";
+	}//if
+	if(cb[3] == "X" && cb[6] == "" && cb[9] == "X"){
+		return "six";
+	}//if
+	if(cb[3] == "" && cb[6] == "X" && cb[9] == "X"){
+		return "three";
+	}//if
+
+
+
+	//top left to bottom right
+	if(cb[1] == "X" && cb[5] == "X" && cb[9] == ""){
+		return "nine";
+	}//if
+	if(cb[1] == "X" && cb[5] == "" && cb[9] == "X"){
+		return "five";
+	}//if
+	if(cb[1] == "" && cb[5] == "X" && cb[9] == "X"){
+		return "one";
+	}//if
+
+	//top right to bottom left
+	if(cb[3] == "X" && cb[5] == "X" && cb[7] == ""){
+		return "seven";
+	}//if
+	if(cb[3] == "X" && cb[5] == "" && cb[7] == "X"){
+		return "five";
+	}//if
+	if(cb[3] == "" && cb[5] == "X" && cb[7] == "X"){
+		return "three";
+	}//if
+
+	return "";
+}//stopWinningMove
